725ddf8713ac3f682c2a0e7e314f961ad541f0f6
Eva <anaevanilde@gmail.com>
2021-07-11 21:37:10 -0300

    Change default time method of appointment factory

--- test/factories/appointment_factory.rb
+++ test/factories/appointment_factory.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :appointment do
     responsible_name { 'responsible' }
-    start_time { default_start_time }
-    end_time { default_start_time + 1.hour }
+    start_time { appointment_default_start_time }
+    end_time { appointment_default_start_time + 1.hour }
     room { build(:room) }
   end
 end
 
-private
-def default_start_time
+def appointment_default_start_time
   Time.current.since(1.day).beginning_of_day + 10.hour
 end
