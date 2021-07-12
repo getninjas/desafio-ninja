830def16991da619dd6fface95037bcb5cad4c4f
Eva <anaevanilde@gmail.com>
2021-07-12 12:40:31 -0300

    Overide root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
   # Returns not found error in api standard instead an ugly exception
   rescue_from ActiveRecord::RecordNotFound do |_|
-    render json: { errors: ['Record not found'] }, status: 404
+    render json: { errors: ['Record not found'] }, status: :not_found
+  end
+
+  def index
+    render json: {
+      message: 'This is an api only app, please see documentation:'\
+               'https://documenter.getpostman.com/view/8419256/Tzm8FadD'
+    }
   end
 end
