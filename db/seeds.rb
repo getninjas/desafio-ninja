7e1a3f4df41c6cf7ae95602152c65cb5b3e4e88e
Fabio Muller <fabioml10@yahoo.com.br>
2021-08-02 16:28:49 -0300

    Add rooms to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "Creating rooms..."
+["Blue Room", "Yellow Room Two", "Red Room", "Green Room"].each do |room|
+  Room.create(name: room)
+end
+p "Done."
