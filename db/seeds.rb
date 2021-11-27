df2a3500188416f4c09c95706113e264a7f05c84
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-11-27 10:48:32 -0300

    feat: adds meetings in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+puts "Starting seed process..."
 titles = ['QG Ninja 1', 'QG Ninja 2', 'QG Ninja 3', 'QG Ninja 4']
 
 titles.each do |title|
-  Room.create(name: title)
+  room = Room.create(name: title)
+  starts_at = DateTime.new(2021, 11, 26, 10, 0, 0)
+  ends_at = DateTime.new(2021, 11, 26, 11, 0, 0)
+  rand_time = rand(30..90).minutes
+
+  Meeting.create(title: 'Comunicado Importante', starts_at: starts_at + rand_time, ends_at: ends_at + rand_time, room: room)
 end
+
+puts "Finished seed process!"
