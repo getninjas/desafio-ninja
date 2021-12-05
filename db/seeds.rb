9e9789cbbd76d42c40f0e2f1974c11ab783f440d
Milles Dyson <milles.schroeder@gmail.com>
2021-12-05 14:19:05 -0300

    Populando banco com db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Criação da sede São Paulo
+organization = Organization.create(name: "GetNinjas São Paulo", adress: "São Paulo-SP", number_of_rooms: 4, work_on_weekend: false, business_hours_start: "09:00", business_hours_end: "18:00")
+
+4.times do |room|
+  Room.create(name: "Sala #{room + 1}", organization_id: organization.id )
+end
