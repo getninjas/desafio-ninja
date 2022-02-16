f1cd5185fde0a8ac08613a175ae8808fe05903f6
Adenilson Santos <adenilsonblog@gmail.com>
2022-02-16 00:27:08 -0300

    generated scaffold for EMPLOYEE table

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,51 @@
+class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :update, :destroy]
+
+  # GET /employees
+  def index
+    @employees = Employee.all
+
+    render json: @employees
+  end
+
+  # GET /employees/1
+  def show
+    render json: @employee
+  end
+
+  # POST /employees
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      render json: @employee, status: :created, location: @employee
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /employees/1
+  def update
+    if @employee.update(employee_params)
+      render json: @employee
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /employees/1
+  def destroy
+    @employee.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def employee_params
+      params.require(:employee).permit(:name, :department)
+    end
+end
---
+++ db/migrate/20220216032633_create_employees.rb
@@ -0,0 +1,10 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :department
+
+      t.timestamps
+    end
+  end
+end
