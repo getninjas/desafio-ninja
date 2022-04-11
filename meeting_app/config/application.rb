d88e6de11eb61a524d7330bccf76de9a344afed4
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2022-04-11 01:55:12 -0300

    Create pt-br file and configs

--- meeting_app/config/application.rb
+++ meeting_app/config/application.rb
@@ -27,6 +27,12 @@ module MeetingApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # The default locale is :"pt-BR" and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.locale = :"pt-BR"
+    config.i18n.available_locales = [:"pt-BR"]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
