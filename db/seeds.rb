25c0200b0d27a027e57c1227f81c7c22c4437f86
Italo Fasanelli <italo.miranda@locaweb.com.br>
2022-05-14 01:02:27 -0300

    Adds data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+room1 = Room.create(name: 'Sala 1')
+room2 = Room.create(name: 'Sala 2')
+room3 = Room.create(name: 'Sala 3')
+room4 = Room.create(name: 'Sala 4')
+user1 = User.create(name: 'Italo', email: 'italo@getninjas.com.br', password: '123456')
+user2 = User.create(name: 'Mariana', email: 'mariana@getninjas.com.br', password: '123456')
+user3 = User.create(name: 'Aurora', email: 'aurora@getninjas.com.br', password: '123456')
+start_time = DateTime.now - 15.minutes
+end_time = DateTime.now + 15.minutes
+
+meeting = Meeting.new(
+  start_time: start_time,
+  end_time: end_time,
+  user_id: user1.id,
+  room: room1,
+  users: [user2, user3]
+)
+meeting.save
+
