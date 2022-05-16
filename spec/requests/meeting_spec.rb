47d173b51afb55f41004621e2bd358bf6f306d45
Italo Fasanelli <italo.miranda@locaweb.com.br>
2022-05-16 16:16:57 -0300

    Adds specs to actions #my_created_meetings and #my_meetings_invitations

--- spec/requests/meeting_spec.rb
+++ spec/requests/meeting_spec.rb
@@ -1,31 +1,92 @@
 require 'rails_helper'
+require 'webmock/rspec'
+include ActionController::RespondWith
 
 RSpec.describe "Meetings", type: :request do
-  describe "GET /my_meetings" do
-    it "returns http success" do
-      get "/my_meetings"
-      expect(response).to have_http_status(:success)
+  let(:response_empty_created_meetings) do
+    { message: "You don't have created meetings yet." }.to_json
+  end
+  let(:response_empty_invited_meetings) do
+    { message: "You don't have any inveted meetings yet." }.to_json
+  end
+
+  before(:each) do
+    @current_user = create(:user)
+    login
+    @auth_params = get_auth_params_from_login_response_headers(response)
+  end
+
+  describe "GET /my_created_meetings" do
+    context "when user does not have any created meetings" do
+      it "returns an info message and http success" do
+        get api_my_created_meetings_path, headers: @auth_params
+
+        expect(response).to have_http_status(:success)
+        expect(response.body). to eq(response_empty_created_meetings)
+      end
+    end
+
+    context "when user has a created meeting" do
+      it "returns an info message and http success" do
+        meeting = create(:meeting, user_id: @current_user.id)
+        invited_user = create(:user, name: 'Aurora', email: 'aurora@email.com')
+        other_invited_user = create(:user, name: 'Mariana', email: 'mariana@email.com')
+        meeting.users << [invited_user, other_invited_user]
+        get api_my_created_meetings_path, headers: @auth_params
+
+        expect(response).to have_http_status(:success)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+        meeting_data = data[:my_created_meetings].first
+        expect(date_formatter(meeting_data[:start_time])).to eq(meeting.start_time.strftime("%H:%M %d/%m/%Y"))
+        expect(date_formatter(meeting_data[:end_time])).to eq(meeting.end_time.strftime("%H:%M %d/%m/%Y"))
+        expect(meeting_data[:room]).to eq(meeting.room.name)
+        expect(meeting_data[:subject]).to eq(meeting.subject)
+        expect(meeting_data[:participants]).to match_array(
+          [
+            { email: invited_user.email, name: invited_user.name },
+            { email: other_invited_user.email, name: other_invited_user.name }
+          ]
+        )
+      end
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/meeting/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "GET /my_meetings_invitations" do
+    context "when user does not have any invited meetings" do
+      it "returns http success" do
+        get api_my_meetings_invitations_path, headers: @auth_params
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/meeting/update"
-      expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:success)
+        expect(response.body). to eq(response_empty_invited_meetings)
+      end
     end
-  end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/meeting/show"
-      expect(response).to have_http_status(:success)
+    context "when user has an invited meeting" do
+      it "returns an info message and http success" do
+        meeting_owner = create(:user, name: 'Mariana', email: 'mariana@email.com')
+        meeting = create(:meeting, user_id: meeting_owner.id)
+        invited_user = create(:user, name: 'Aurora', email: 'aurora@email.com')
+        meeting.users << [invited_user, @current_user]
+        get api_my_meetings_invitations_path, headers: @auth_params
+
+        expect(response).to have_http_status(:success)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+        meeting_data = data[:my_meetings_invitations].first
+        expect(date_formatter(meeting_data[:start_time])).to eq(meeting.start_time.strftime("%H:%M %d/%m/%Y"))
+        expect(date_formatter(meeting_data[:end_time])).to eq(meeting.end_time.strftime("%H:%M %d/%m/%Y"))
+        expect(meeting_data[:room]).to eq(meeting.room.name)
+        expect(meeting_data[:owner_name]).to eq(meeting_owner.name)
+        expect(meeting_data[:owner_email]).to eq(meeting_owner.email)
+        expect(meeting_data[:subject]).to eq(meeting.subject)
+        expect(meeting_data[:participants]).to match_array(
+          [
+            { email: @current_user.email, name: @current_user.name },
+            { email: invited_user.email, name: invited_user.name }
+          ]
+        )
+      end
     end
   end
 
