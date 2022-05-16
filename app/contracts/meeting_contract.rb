5b9567af857f2d5869a27ea577acee42f86f5224
Italo Fasanelli <italo.miranda@locaweb.com.br>
2022-05-16 00:14:54 -0300

    Adds rules to meeting contract

--- app/contracts/meeting_contract.rb
+++ app/contracts/meeting_contract.rb
@@ -1,7 +1,24 @@
 class MeetingContract < Dry::Validation::Contract
   params do
-    required(:start_time).filled(:date)
-    required(:end_time).filled(:date)
-    required(:users_emails).array(:string)
+    required(:start_time).filled(:time)
+    required(:end_time).filled(:time)
+    optional(:subject).maybe(:string)
+    optional(:users_emails).array(:string)
+  end
+
+  rule(:start_time) do
+    key.failure('creating events is allowed only on weekdays') if value.saturday? || value.sunday?
+    key.failure('creating events is allowed only until 5:59pm') if value.strftime('%H').to_i >= 18
+    key.failure('creating events is allowed only before 9am') if value.strftime('%H').to_i <= 9
+  end
+
+  rule(:end_time) do
+    key.failure('creating events is allowed only on weekdays') if value.saturday? || value.sunday?
+    key.failure('creating events is allowed only until 5:59pm') if value.strftime('%H').to_i > 18
+    key.failure('creating events is allowed only before 9am') if value.strftime('%H').to_i < 9
+  end
+
+  rule(:start_time, :start_time) do
+    key.failure('must be before end_time') if values[:start_time] >= values[:start_time]
   end
 end
