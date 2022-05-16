e8df6e1bb56cb3ac23129511bdeaa05bedf9fe63
Italo Fasanelli <italo.miranda@locaweb.com.br>
2022-05-16 17:03:35 -0300

    Adds spec helper methods

--- spec/requests/meeting_spec.rb
+++ spec/requests/meeting_spec.rb
@@ -94,7 +94,29 @@ RSpec.describe "Meetings", type: :request do
     it "returns http success" do
       get "/meeting/delete"
       expect(response).to have_http_status(:success)
-    end
+
+  def login
+    post api_user_session_path, params:  { email: @current_user.email, password: 'password' }.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
   end
 
+  def get_auth_params_from_login_response_headers(response)
+    client = response.headers['client']
+    token = response.headers['access-token']
+    expiry = response.headers['expiry']
+    token_type = response.headers['token-type']
+    uid = response.headers['uid']
+
+    auth_params = {
+      'access-token' => token,
+      'client' => client,
+      'uid' => uid,
+      'expiry' => expiry,
+      'token-type' => token_type
+    }
+    auth_params
+  end
+
+  def date_formatter(date)
+    DateTime.parse(date).strftime("%H:%M %d/%m/%Y")
+  end
 end
