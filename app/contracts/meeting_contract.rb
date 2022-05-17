40420e7c8020dac5f933cf3fec7e08973f01687d
Italo Fasanelli <italo.miranda@locaweb.com.br>
2022-05-17 00:19:59 -0300

    Adds fixes to contract

--- app/contracts/meeting_contract.rb
+++ app/contracts/meeting_contract.rb
@@ -7,20 +7,20 @@ class MeetingContract < Dry::Validation::Contract
   end
 
   rule(:start_time) do
-    key.failure('creating events is allowed only on weekdays') if value.saturday? || value.sunday?
+    key.failure('creating events is allowed only on weekdays.') if value.saturday? || value.sunday?
     key.failure(
-      'creating events is allowed only from 9am to 6pm on weekdays'
-    ) if value.strftime('%H').to_i < 9 && value.strftime('%H').to_i >= 18
+      'creating events is allowed only from 9am to 6pm on weekdays.'
+    ) if value.strftime('%H').to_i < 9 || value.strftime('%H').to_i >= 18
   end
 
   rule(:end_time) do
-    key.failure('creating events is allowed only on weekdays') if value.saturday? || value.sunday?
+    key.failure('creating events is allowed only on weekdays.') if value.saturday? || value.sunday?
     key.failure(
-      'creating events is allowed only from 9am to 6pm on weekdays'
-    ) if value.strftime('%H').to_i < 9 && value.strftime('%H').to_i > 18
+      'creating events is allowed only from 9am to 6pm on weekdays.'
+    ) if value.strftime('%H').to_i < 9 || value.strftime('%H').to_i > 18
   end
 
-  rule(:start_time, :start_time) do
-    key.failure('must be before end_time') if values[:start_time] < values[:start_time]
+  rule(:start_time, :end_time) do
+    key.failure('must be before end_time.') if values[:start_time] >= values[:end_time]
   end
 end
