6b5bd9c49072d120ad6f183fa1fdfb25a94ad4ac
Italo Fasanelli <italo.miranda@locaweb.com.br>
2022-05-17 00:20:24 -0300

    Adds fixes to time format

--- spec/factories/meetings.rb
+++ spec/factories/meetings.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :meeting do
-    start_time { '2022-05-16 00:49:05' }
-    end_time { '2022-05-16 00:49:05' }
+    start_time { '2022-05-16T14:41:02.827Z' }
+    end_time { '2022-05-16T15:11:02.827Z' }
     subject { 'Planning' }
     room
     user
